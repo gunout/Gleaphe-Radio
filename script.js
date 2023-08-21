9fd615773e110b3cfdeebfe374ce31c843f900c3
gunout <88831335+gunout@users.noreply.github.com>
2023-08-22 01:29:50 +0400

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $("#btn").on("click", function() {
-    $("#video").html("<source src='"+ $("#url").val() +"' type='video/mp4'>");
-    var ply = videojs("video");
-    ply.play();
-  });
-});
