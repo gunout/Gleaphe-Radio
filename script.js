ac17144d8132497b359c5fde156cfea3f258ffa4
gunout <88831335+gunout@users.noreply.github.com>
2023-08-25 20:35:17 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+function getLocalStream() {
+  navigator.mediaDevices
+    .getUserMedia({ video: false, audio: true })
+    .then((stream) => {
+      window.localStream = stream;
+      window.localAudio.srcObject = stream;
+      window.localAudio.autoplay = true;
+    })
+    .catch((err) => {
+      console.error(`you got an error: ${err}`);
+    });
+}
+
+getLocalStream();
+
 //Status constants
 var SESSION_STATUS = Flashphoner.constants.SESSION_STATUS;
 var STREAM_STATUS = Flashphoner.constants.STREAM_STATUS;
